c4a62cb59e003d91a674a4c3c1e854bbd6961b38
Monoarul Islam Moon <64534598+MoonMonoar@users.noreply.github.com>
2023-07-18 01:05:16 +0600

    Authentication done

--- app/src/main/java/com/immo2n/alquran/Splash.java
+++ app/src/main/java/com/immo2n/alquran/Splash.java
@@ -1,14 +1,13 @@
 package com.immo2n.alquran;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_confirm_phone);
     }
 }
