2a2586b2fb09125ebebc7aaf3be59742fdd9dd0e
Monoarul Islam Moon <64534598+MoonMonoar@users.noreply.github.com>
2023-07-18 21:01:36 +0600

    Home design

--- app/src/main/java/com/immo2n/alquran/Splash.java
+++ app/src/main/java/com/immo2n/alquran/Splash.java
@@ -8,6 +8,6 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirm_phone);
+        setContentView(R.layout.activity_home);
     }
 }
