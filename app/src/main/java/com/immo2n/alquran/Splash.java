71e20155396150d4606ff7b72b142605d5adbd24
Monoarul Islam Moon <64534598+MoonMonoar@users.noreply.github.com>
2023-07-21 23:05:47 +0600

    Home design - Bottom 4 type

--- app/src/main/java/com/immo2n/alquran/Splash.java
+++ app/src/main/java/com/immo2n/alquran/Splash.java
@@ -8,6 +8,6 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_prev);
+        setContentView(R.layout.activity_splash);
     }
 }
