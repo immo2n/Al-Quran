5c3e37dd50cc67ca4cbcaa70adcf538d47021272
Rakibul Islam <rakibull2000@gmail.com>
2023-07-28 14:43:05 +0600

    Add tab switcher (TabLayout) for library_page.xml

--- app/src/main/java/com/immo2n/alquran/Splash.java
+++ app/src/main/java/com/immo2n/alquran/Splash.java
@@ -9,7 +9,7 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        startActivity(new Intent(Splash.this, Note.class));
+        setContentView(R.layout.library_page);
+        // startActivity(new Intent(Splash.this, Note.class));
     }
 }
